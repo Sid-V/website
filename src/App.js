31825d1a38961b2f11a794e2e73e4b102f3d4687
Sid-V <sidv@purdue.edu>
2020-01-22 16:45:15 -0500

    fix

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch, HashRouter } from "react-router-dom";
+import { Divider } from "semantic-ui-react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import WelcomeMessage from "./Welcome.js";
+import Projects from "./Projects.js";
+import Resume from "./Resume.js";
+import Navbar from "./Navbar.js";
+import Home from "./Home.js";
+import Experience from "./Experience.js";
+import Links from "./Links.js";
+import Gallery from "./Gallery.js";
+
+//TODO
+//Add WelcomeMessage later
+//Add video link for twister
+//Add TA experience tab
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Router basename={process.env.PUBLIC_URL}>
+          <Navbar />
+          <Divider hidden />
+          <div className="App-content">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/gallery" component={Gallery} />
+              <Route path="/experience" component={Experience} />
+              <Route path="/projects" component={Projects} />
+              <Route path="/resume" component={Resume} />
+            </Switch>
+          </div>
+          <Divider />
+          <Links />
+        </Router>
+      </div>
+    );
+  }
 }
-
-export default App;
